051738eb88c0a421747283b5b7c904f74d2b11e6
Dmitry Udalov <udalov@altlinux.org>
2026-02-02 13:01:17 +0600

    fix: dont use prefix on export/unexport feat

--- internal/embed/embed.go
+++ internal/embed/embed.go
@@ -19,14 +19,14 @@ var utilsTarGz []byte
 
 func GetBash() ([]byte, error) {
 	if len(bashBinary) == 0 {
-		return nil, fmt.Errorf("bash binary not embedded - run 'make deps' first")
+		return nil, fmt.Errorf("bash binary not embedded")
 	}
 	return bashBinary, nil
 }
 
 func GetUtils() ([]byte, error) {
 	if len(utilsTarGz) == 0 {
-		return nil, fmt.Errorf("utils tarball not embedded - run 'make deps' first")
+		return nil, fmt.Errorf("utils tarball not embedded")
 	}
 	return utilsTarGz, nil
 }
